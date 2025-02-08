1352ba233ab53f4504c9677fe964938e45dbe3df
wjb <wjbryson@gmail.com>
2025-02-08 13:23:17 -0500

    gptUpdate_dmg.js

--- scripts/dynamic-menu-grid.js
+++ scripts/dynamic-menu-grid.js
@@ -1,46 +1,36 @@
-// generate daily subgrids; these act as draggable containers for each day's menu items
-for (let i = 0; i < 7; i++) {
-  const dailySubGrid = document.createElement("div");
+// ***MODERNIZED DYNAMIC MENU GRID***
+
+document.addEventListener("DOMContentLoaded", () => {
   const subGrid = document.querySelector("#subGrid");
+  
+  for (let i = 0; i < 7; i++) {
+      const dailySubGrid = document.createElement("div");
+      dailySubGrid.className = "daySubGrid";
+      dailySubGrid.id = `dsg${i + 1}`;
+      subGrid.appendChild(dailySubGrid);
 
-  // constants and variables for dynamic SVG creation
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+      for (let j = 0; j < 4; j++) {
+          const menuItem = document.createElement("input");
+          menuItem.className = "menuItems";
+          menuItem.id = `mi${i + 1}-${j + 1}`;
+          dailySubGrid.appendChild(menuItem);
+      }
 
-  //dailySubGrid.setAttribute("draggable", true);
-  dailySubGrid.className = "daySubGrid";
-  dailySubGrid.id = "dsg" + [i + 1];
-  subGrid.appendChild(dailySubGrid);
-
-  for (let j = 0; j < 4; j++) {
-    const menuItem = document.createElement("input");
-    const currentDSG = document.querySelector("#dsg" + [i + 1]);
-
-    // menuItem.setAttribute("contentEditable", false);
-    menuItem.className = "menuItems";
-    menuItem.id = "mi" + [j + 1];
-    currentDSG.appendChild(menuItem);
-    for (let k = 0; k < 1; k++) {
+      // SVG Icon Creation
+      const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
       svg.setAttribute("aria-hidden", "true");
-      // svg.setAttribute(
-      //   "style",
-      //   "-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"
-      // );
       svg.setAttribute("preserveAspectRatio", "xMidYMid meet");
-      svg.setAttribute("viewbox", "0 -10 24 24");
+      svg.setAttribute("viewBox", "0 -10 24 24");
       svg.setAttribute("width", "5em");
       svg.setAttribute("height", "5em");
-      svg.id = "svg" + [i + 1];
-      svg.className.baseVal = "handleSVG";
-
-      path.setAttribute(
-        "d",
-        "M19 9H5c-.55 0-1 .45-1 1s.45 1 1 1h14c.55 0 1-.45 1-1s-.45-1-1-1zM5 15h14c.55 0 1-.45 1-1s-.45-1-1-1H5c-.55 0-1 .45-1 1s.45 1 1 1z"
-      );
+      svg.id = `svg${i + 1}`;
+      svg.classList.add("handleSVG");
+      
+      const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+      path.setAttribute("d", "M19 9H5c-.55 0-1 .45-1 1s.45 1 1 1h14c.55 0 1-.45 1-1s-.45-1-1-1zM5 15h14c.55 0 1-.45 1-1s-.45-1-1-1H5c-.55 0-1 .45-1 1s.45 1 1 1z");
       path.setAttribute("fill", "#626262");
-
+      
       svg.appendChild(path);
-      currentDSG.appendChild(svg);
-    }
+      dailySubGrid.appendChild(svg);
   }
-}
+});
